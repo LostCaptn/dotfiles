4c35504965e1777d50f7a6dd702e1f65e6673b8d
LostCaptn <c.nelson.ap@gmail.com>
2025-02-26 17:23:37 +0100

    adding nvim config

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,14 @@
+vim.wo.number = true -- make line numbers default (default: false)
+vim.o.relativenumber = true -- Set relative numbered lines (default: false)
+vim.o.clipboard = 'unnamedplus' --Sync clipboard between OS and Neovim
+vim.o.wrap = false -- Display lines as one long line (default: true)
+vim.o.linebreak = true -- COmpanion to wrap, don't split words
+vim.o.mouse = 'a' -- Enable mouse mode
+vim.o.undofile = true -- Save undo history
+vim.o.tabstop = 4 --insert n spaces for a tab
+vim.o.softtabstop = 4 -- humber of spaces that a tab counts for while performing editing operations
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.shiftwidth = 4 -- number of spaces inserted for each indentation
+vim.o.autoindent = true -- copy indent from current line when starting new one (default: true)
+vim.o.ignorecase = true --case-insensitive searching unless \C or capital in search (default: false)
+
